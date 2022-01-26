915d76be90a47a0a630a4dcffecc666d10803584
theGDM <gyandeepmehra370@gmail.com>
2022-01-26 11:47:28 +0530

    Create Check If Circular Linked List.java

---
+++ LinkedList/Check If Circular Linked List.java
@@ -0,0 +1,37 @@
+/* Structure of LinkedList
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+*/
+
+class GfG{
+    boolean isCircular(Node head){
+    //0 or 1 node without loop
+	if(head == null || head.next == null){
+	    return false;
+	}
+	
+	//self referential node, 1 node with loop
+	if(head.next == head){
+	    return true;
+	}
+	
+	Node slow = head, fast = head;
+	while(fast != null && fast.next != null){
+	    slow = slow.next;
+	    fast = fast.next.next;
+	    
+	    if(slow == head){
+	        return true;
+	    }
+    }
+    return false;
+    }
+}

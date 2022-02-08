6436ce718a53175e682f089250d4ef1e3e7914b3
theGDM <gyandeepmehra370@gmail.com>
2022-02-08 16:45:58 +0530

    Create Implement stack using array.java

---
+++ Stacks & Queues/Implement stack using array.java
@@ -0,0 +1,29 @@
+class MyStack{
+    int top;
+	int arr[] = new int[1000];
+
+    MyStack(){
+		top = -1;
+	}
+	
+	//Function to push an integer into the stack.
+    void push(int a){
+	    // Your code here
+	    if(top == arr.length - 1){
+	        return;
+	    }
+	    ++top;
+	    arr[top] = a;
+	} 
+	
+    //Function to remove an item from top of the stack.
+	int pop(){
+        // Your code here
+        if(top == -1){
+            return -1;
+        }
+        int val = arr[top];
+        --top;
+        return val;
+	}
+}

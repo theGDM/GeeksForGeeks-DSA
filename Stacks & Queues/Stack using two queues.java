0ba396e9e621e572c4bec08c1726c563a912c0d9
theGDM <gyandeepmehra370@gmail.com>
2022-02-09 10:15:33 +0530

    Create Stack using two queues.java

---
+++ Stacks & Queues/Stack using two queues.java
@@ -0,0 +1,24 @@
+class Queues{
+    Queue<Integer> q1 = new LinkedList<Integer>();
+    Queue<Integer> q2 = new LinkedList<Integer>();
+    
+    //Function to push an element into stack using two queues.
+    void push(int a){
+        while(q1.size() > 0){
+            q2.add(q1.remove());
+        }
+        q1.add(a);
+        while(q2.size() > 0){
+            q1.add(q2.remove());
+        }
+    }
+    
+    //Function to pop an element from stack using two queues. 
+    int pop(){
+        if(q1.size() == 0){
+            return -1;
+        }
+	    return q1.remove();
+    }
+	
+}

c7852a15cb7ecff77b7e983045c8ab2c5229932e
theGDM <gyandeepmehra370@gmail.com>
2022-02-16 12:00:53 +0530

    Update Boundary traversal of matrix.java

--- 2D - Array/Boundary traversal of matrix.java
+++ 2D - Array/Boundary traversal of matrix.java
@@ -1,3 +1,23 @@
+// You are given a matrix of dimensions n x m. The task is to perform boundary traversal on the matrix in a clockwise manner.
+
+// Example 1:
+
+// Input:
+// n = 4, m = 4
+// matrix[][] = {{1, 2, 3, 4},
+//          {5, 6, 7, 8},
+//          {9, 10, 11, 12},
+//          {13, 14, 15,16}}
+// Output: 1 2 3 4 8 12 16 15 14 13 9 5
+// Explanation:
+// The matrix is:
+// 1 2 3 4
+// 5 6 7 8
+// 9 10 11 12
+// 13 14 15 16
+// The boundary traversal is:
+// 1 2 3 4 8 12 16 15 14 13 9 5
+
 class Solution{
     //Function to return list of integers that form the boundary 
     //traversal of the matrix in a clockwise manner.

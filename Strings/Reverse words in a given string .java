27f71f2f9c6669f32a5f7d8c48b10f44370aaf56
theGDM <gyandeepmehra370@gmail.com>
2022-02-22 22:51:27 +0530

    Update Reverse words in a given string .java

--- Strings/Reverse words in a given string .java
+++ Strings/Reverse words in a given string .java
@@ -1,3 +1,23 @@
+// Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+
+// Example 1:
+
+// Input:
+// S = i.like.this.program.very.much
+// Output: much.very.program.this.like.i
+// Explanation: After reversing the whole
+// string(not individual words), the input
+// string becomes
+// much.very.program.this.like.i
+// Example 2:
+
+// Input:
+// S = pqr.mno
+// Output: mno.pqr
+// Explanation: After reversing the whole
+// string , the input string becomes
+// mno.pqr
+
 class Solution {
     //Function to reverse words in a given string.
     String reverseWords(String s){
